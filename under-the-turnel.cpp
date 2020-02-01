84787644ca135b4718c82b3cae73f101ec1b9891
Minh Trần Nhật <minh.tn.hust@gmail.com>
2020-02-01 08:39:11 +0700

    New Exercise

---
+++ under-the-turnel.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()	
+	{
+	short 	T;
+	int N,K;
+	short Matran[1000][1000];
+	scanf("%d%d",&N,&K);
+	int rows,columns;
+	for (columns=0;columns<N;columns++)
+	for (rows=0;rows<N;rows++)
+	scanf("%hi",Matran[rows][columns]);
+	}
