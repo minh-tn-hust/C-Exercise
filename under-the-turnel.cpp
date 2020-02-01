fabe34c4fe02bb9989f7125c03c5f20a3703f263
Minh Trần Nhật <minh.tn.hust@gmail.com>
2020-02-01 09:07:08 +0700

    Input

--- under-the-turnel.cpp
+++ under-the-turnel.cpp
@@ -1,13 +1,28 @@
 #include <stdio.h>
-
+#include <string.h>
+void input()
+	{
+		scanf("%d%d",&N,&K);
+		for (count1 = 0;count1<N;count1++)
+		{
+		gets(str);
+		for (count2 = 0; count2 < strlen(str);count2++)
+			{
+			int c;
+			c = str[count2];
+			Matran[count2][count1] = c-48;
+			}
+		}
+	}
 int main()	
 	{
 	short 	T;
 	int N,K;
 	short Matran[1000][1000];
-	scanf("%d%d",&N,&K);
-	int rows,columns;
-	for (columns=0;columns<N;columns++)
-	for (rows=0;rows<N;rows++)
-	scanf("%hi",Matran[rows][columns]);
+	char str[1000];
+	int count1,count2;
+	// Nhap du lieu dua ve ma tran
+	input();
+	
+	
 	}
