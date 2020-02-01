fce359ec2f9b443fbc18a414ff5652a9f62e3c63
Minh Trần Nhật <minh.tn.hust@gmail.com>
2020-02-01 21:20:53 +0700

    Radix Sort - no Complete

--- under-the-turnel.cpp
+++ under-the-turnel.cpp
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <string.h>
-void input()
+int main()	
 	{
-		scanf("%d%d",&N,&K);
-		for (count1 = 0;count1<N;count1++)
+	short 	T;
+	int N,K;
+	short Matran[1000][1000];
+	char str[1000];
+	int count1,count2;
+	// Nhap du lieu dua ve ma tran
+	scanf("%d%d",&N,&K);
+	for (count1 = 0;count1<N;count1++)
 		{
 		gets(str);
 		for (count2 = 0; count2 < strlen(str);count2++)
@@ -13,16 +19,11 @@ void input()
 			Matran[count2][count1] = c-48;
 			}
 		}
-	}
-int main()	
+	for (count1 =0;count1<N;count1++)
 	{
-	short 	T;
-	int N,K;
-	short Matran[1000][1000];
-	char str[1000];
-	int count1,count2;
-	// Nhap du lieu dua ve ma tran
-	input();
-	
+	for (count2 =0;count2<N;count2++)
+	printf("%hi",Matran[count2][count1]);
+	printf("\n");
+	}
 	
 	}
