16e16be27741df3756299c13a90b94929556a4cb
Minh Trần Nhật <minh.tn.hust@gmail.com>
2020-02-03 08:14:14 +0700

    Add some comment

--- bob-and-his-friends.c
+++ bob-and-his-friends.c
@@ -14,13 +14,15 @@ int main()
                 for (count2=0;count2<N;count2++)
                     {
                         scanf("%li",&friends);
+                         // Tat ca moi tang deu su dung con thuc nay de tinh
                         timeFromThisToA = abs(b-friends)+abs(friends-a)+c;
                         if (count2==0) min = timeFromThisToA;
+                        // Tim min luon
                         if (timeFromThisToA < min) min = timeFromThisToA;
 					      
                     }
                 result[count] = min;
             }
-        for (count = 0;count<T;count++) printf("%lli	 \n",result[count]);
+        for (count = 0;count<T;count++) printf("%lli\n",result[count]);
 	
     }
