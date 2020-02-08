eb3a64298720324de2e19f351f29a98f47d29f00
Minh Trần Nhật <minh.tn.hust@gmail.com>
2020-02-08 18:02:13 +0700

    New Radix sort Version with Lager number

--- codechef-SNUG_FIT.c
+++ codechef-SNUG_FIT.c
@@ -15,35 +15,62 @@ int layChuSoThuN(long long radix,int n) // Ham tra ve chu so thu N =============
 	{	
 	return (radix/soMu(n-1))%10;
 	}
-long long maxArray(int AorB) // Tra ve phan tu lon nhat trong 
+long long maxArray(int AorB) // Tra ve phan tu lon nhat trong =======================================================================
 	{
 	int count,max = A[AorB][0];
 	for (count = 0;count < N;count ++)
 		{
-		if (A[AorB][count] > max) max = A[AorB][cout];
+		if (A[AorB][count] > max) max = A[AorB][count];
 		}
 	return max;	
 	}
-int soChuSo(int checked)
+int soChuSo(long long checked) // Tra ve so chu so ==================================================================================
 	{
-	int dem;
-	while (checked > 1)
+	int dem=0;
+	while (checked >= 1)
 		{
-		c
+		dem++;
+		checked = checked/10;
 		}
+	return dem;
 	}
-//void sortA(int soLuong) 
-//	{
-//	int count,vitri;
-//	for (vitri = )
-//	for (count=0;count<N;count++)
-//		{
-//		
-//		}
-//	}
+void connect(int AorB)
+	{
+	int count,dem=0,vitri=0;
+	for (count = 0;count < N;count++)
+		{
+		if (locate[vitri] == 0) vitri++;
+		else
+			{
+			A[AorB][count] = phanChia[vitri][dem];
+			dem++;
+			if (dem == locate[vitri]) {vitri++;dem=0;}
+			}
+		}
+	for (count = 0;count<10;count++) locate[count] = 0;
+	}
+void divAorB(int AorB) // 
+	{
+	int count1,count2,chuSo,vitri = soChuSo(maxArray(AorB));  // count : dem phan tu mang ;  vitri : dem vi tri cua chu so
+	for (count1 = 1;count1 <=vitri;count1++)
+	{
+	for (count2=0;count2<N;count2++)
+		{
+		chuSo = layChuSoThuN(A[AorB][count2],count1);
+		phanChia[chuSo][locate[chuSo]] = A[AorB][count2];
+		locate[chuSo]++;		
+		}
+	connect(AorB);
+	}
+	}
 int main()
 	{
-	
+		N = 10;
+		int count;
+		for (count = 0; count < N ;count ++) scanf("%lli",&A[0][count]);
+		for (count = 0; count < N ;count ++) scanf("%lli",&A[1][count]);
+		divAorB(0);
+		printf("Hello World");
 //	long long s=0;
 //	int T,N;
 //	scanf("%d",&T);
