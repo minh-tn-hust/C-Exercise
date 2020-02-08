984a8bfa9c3442e4422d163f687f9f84d5bf3142
Minh Trần Nhật <minh.tn.hust@gmail.com>
2020-02-08 21:15:05 +0700

    100% - Radix sort by my seft - 20000 in 0,21s

--- codechef-SNUG_FIT.c
+++ codechef-SNUG_FIT.c
@@ -2,10 +2,10 @@
 #include <math.h>
 long long result[50];
 long long A[2][10000];
-long long phanChia[10][10000];
-long long locate[10] = {0,0,0,0,0,0,0,0,0,0};
+long long phanChia[10][10000]; // chia cac chu so tu 0-10 vao cac so tren
+long long locate[10] = {0,0,0,0,0,0,0,0,0,0}; // Luu lai so luong phan tu moi mang sau khi chia
 int N;
-int soMu(int coso) // Ham lay so mu =================================================================================================
+int soMu(int coso) // Ham lay so mu co so 10=================================================================================================
 	{
 	int count,tich=1;
 	for (count = 0;count < coso;count++) tich*=10;
@@ -15,7 +15,7 @@ int layChuSoThuN(long long radix,int n) // Ham tra ve chu so thu N =============
 	{	
 	return (radix/soMu(n-1))%10;
 	}
-long long maxArray(int AorB) // Tra ve phan tu lon nhat trong =======================================================================
+long long maxArray(int AorB) // Tra ve phan tu lon nhat trong mang=======================================================================
 	{
 	int count,max = A[AorB][0];
 	for (count = 0;count < N;count ++)
@@ -24,6 +24,7 @@ long long maxArray(int AorB) // Tra ve phan tu lon nhat trong ==================
 		}
 	return max;	
 	}
+	
 int soChuSo(long long checked) // Tra ve so chu so ==================================================================================
 	{
 	int dem=0;
@@ -34,14 +35,15 @@ int soChuSo(long long checked) // Tra ve so chu so =============================
 		}
 	return dem;
 	}
-void connect(int AorB) // Ket noi cac phan tu mang= =================================================================================
+	
+void connect(int AorB) // Ket noi cac phan tu mang thong qua he thong phanChia =================================================================================
 	{
 	int count=0,dem=0,vitri=0;
 	while (count != N)
 		{
-			if (locate[vitri] == 0) vitri++;
+			if (locate[vitri] == 0) vitri++;     // Check so luong phan tu moi ngan
 			else
-			{	if (dem == locate[vitri]) {vitri++;dem=0;}
+			{	if (dem == locate[vitri]) {vitri++;dem=0;} 
 				else
 				{
 				A[AorB][count] = phanChia[vitri][dem];
@@ -52,18 +54,19 @@ void connect(int AorB) // Ket noi cac phan tu mang= ============================
 		}
 	for (count = 0;count<10;count++) locate[count] = 0;
 	}
+	
 void divAorB(int AorB) // Chia cac phan tu mang =====================================================================================
 	{
 	int count1,count2,chuSo,vitri = soChuSo(maxArray(AorB));  // count : dem phan tu mang ;  vitri : dem vi tri cua chu so
-	for (count1 = 1;count1 <=vitri;count1++)
+	for (count1 = 1;count1 <=vitri;count1++) // Vi tri cac chu so
 	{
-	for (count2=0;count2<N;count2++)
+	for (count2=0;count2<N;count2++) // Duyet qua cac phan tu trong mang
 		{
-		chuSo = layChuSoThuN(A[AorB][count2],count1);
-		phanChia[chuSo][locate[chuSo]] = A[AorB][count2];
-		locate[chuSo]++;		
+		chuSo = layChuSoThuN(A[AorB][count2],count1); // Lay chu so 
+		phanChia[chuSo][locate[chuSo]] = A[AorB][count2]; // Ep cho so vao cac ngan
+		locate[chuSo]++;		 // Cung cap so luong moi ngan
 		}
-	connect(AorB);
+	connect(AorB); // Ket noi cac mang
 	}
 	}
 int main()
@@ -76,10 +79,10 @@ int main()
 		{
 		scanf("%d",&N);
 		int count2;
-		for (count2 = 0; count2 < N ;count2 ++) scanf("%lli",&A[0][count2]);
-		for (count2 = 0; count2 < N ;count2 ++) scanf("%lli",&A[1][count2]);
-		divAorB(0);
-		divAorB(1);
+		for (count2 = 0; count2 < N ;count2 ++) scanf("%lli",&A[0][count2]); // Nhap du lieu
+		for (count2 = 0; count2 < N ;count2 ++) scanf("%lli",&A[1][count2]); // Nhap du lieu
+		divAorB(0); // Sort mang A
+		divAorB(1); // Sort mang B
 		for (count2=0;count2<N;count2++)
 			{
 			if (A[0][count2] < A[1][count2]) s+= A[0][count2];
@@ -89,5 +92,5 @@ int main()
 		s=0;
 		}
 	for (count=0;count<T;count++) printf("%lli\n",result[count]);
-
+// Thuat toan nay co the sort 20000 phan tu trong 0,21s
 	}
