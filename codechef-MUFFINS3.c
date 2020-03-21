5a49db71ec143eb8c21f0a572c91287bf54d070e
Minh Trần Nhật <minh.tn.hust@gmail.com>
2020-03-21 18:42:54 +0700

    Nothing

--- codechef-MUFFINS3.c
+++ codechef-MUFFINS3.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+int main()
+	{
+	int T;
+	int count;
+	scanf("%d",&T);
+	for (count = 0; count <T;count++)
+		{
+		long N,check;
+		scanf("%ld",&N);
+		check = N/2;
+		if (N == 1) printf("2");
+		else printf("%ld",check+1);
+		}
+	}
