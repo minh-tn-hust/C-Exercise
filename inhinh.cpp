8ff5e954de8977b862b9ef9444de58afbdfed3bb
Minh Trần Nhật <minh.tn.hust@gmail.com>
2020-03-23 00:42:12 +0700

    Relax coding

---
+++ inhinh.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+char kitu[] = " abcdefghijklmnopqrstuvwxyz";
+int n;
+void ingach(int soluonggach)
+	{
+	int count;
+	for (count = 0; count <soluonggach;count++)
+	printf("-");
+	}
+void inhang(int hangthu)
+	{
+	ingach((4*n-3-(hangthu*4-3))/2);
+	int congtac=1,count,vitri=n;
+	for (count = 0; count < hangthu*2-1;count++)
+		{
+		printf("%c",kitu[vitri]);
+		if (count != hangthu*2-2 ) printf("-");
+		if (congtac == 1) vitri--;
+		if (congtac == 0) vitri++;
+		if (vitri == 1) congtac = 0;
+		}
+	ingach((4*n-3-(hangthu*4-3))/2);
+	printf("\n");
+	}
+int main()
+	{
+	scanf("%d",&n);
+	int count;
+	for (count = 1; count <= 2*n-1; count ++)
+	if (count <= n) inhang(count);
+	else inhang(2*n-count);
+	
+	}
